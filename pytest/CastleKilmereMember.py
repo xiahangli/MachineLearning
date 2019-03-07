cc5afabf75289a9235f3b810a5cdccb06d09b90c
xiahangli <2427417167@qq.com>
2019-03-07 16:53:07 +0800

    Initial commit

---
+++ pytest/CastleKilmereMember.py
@@ -0,0 +1,16 @@
+class CastleKilmereMember:
+    """
+    Creates a member of the Castle Kilmere School of Magic
+    """
+
+    def __init__(self, name, birthyear, sex):
+        self._name = name
+        self.birthyear = birthyear
+        self.sex = sex
+
+    def says(self, words):
+        return f"{self._name} says {words}"
+
+
+bromley = CastleKilmereMember('Bromley Huckabee', '1959', 'male')
+print(bromley.says("Hello!"))
